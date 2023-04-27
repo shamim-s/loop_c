ede3fd98b7e4dc57e06b6eb8af5013688536f7b9
shamim-s <shamimibas@gmail.com>
2023-04-28 00:06:20 +0600

    first commit

---
+++ loop_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int sum = 0;
+    
+    printf("%d\n",sum);
+    return  0;
+}
---
+++ loop_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    if (a%3==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
